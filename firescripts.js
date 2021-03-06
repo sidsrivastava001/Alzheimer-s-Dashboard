a07e1b389d7bcf9109b9c9855e6b95e154af034a
DivBear03 <56279449+DivBear03@users.noreply.github.com>
2021-03-06 15:54:00 -0500

    started skeleton for firescripts.js firebase communication

--- firescripts.js
+++ firescripts.js
@@ -11,3 +11,31 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
+
+//variables
+var something
+
+function dataPrep() {
+
+}
+
+function toast(text){
+  text = "<span>"+String(text)+"</span>";
+  M.toast({html: text});
+}
+//Insert Appointment into the database
+document.getElementById("insert_appointment").onclick = function() {
+  dataPrep();
+  firebase.database().ref('doctor1/Appointments/' + String(Date)).set( {
+    Patient_Name: patient_name,
+    Bucket_Name: bucket_name,
+    Alzheimer_Test_Score: test_score,
+    ML_Score: ml_score,
+    Doctor_Score: doc_score
+  })
+};
+
+document.getElementById('some-other-button').onclick = function() {
+
+};
+
