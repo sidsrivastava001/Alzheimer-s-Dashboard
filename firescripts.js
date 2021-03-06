84554f730e2f02d0ec06216003bf4fc119200df0
DivBear03 <56279449+DivBear03@users.noreply.github.com>
2021-03-06 11:42:56 -0500

    added firebase SDK to firescripts

---
+++ firescripts.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAJkaDmP2xrCFIHzufBhWqKcrRK6kvvtig",
+    authDomain: "tsa-software-group-2.firebaseapp.com",
+    databaseURL: "https://tsa-software-group-2-default-rtdb.firebaseio.com",
+    projectId: "tsa-software-group-2",
+    storageBucket: "tsa-software-group-2.appspot.com",
+    messagingSenderId: "1009383655239",
+    appId: "1:1009383655239:web:7706faf6dcce60f7ea2e62",
+    measurementId: "G-ZXHZ4YWP9V"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
