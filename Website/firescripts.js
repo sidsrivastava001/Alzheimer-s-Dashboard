16375f1a1dffa462b9faf61c7caafe40158bf419
DivBear03 <56279449+DivBear03@users.noreply.github.com>
2021-03-16 18:44:05 -0400

    Materialize toasts not working, can't really debug unless I figure out console

--- Website/firescripts.js
+++ Website/firescripts.js
@@ -13,10 +13,37 @@ var firebaseConfig = {
   firebase.analytics();
 
 //variables
-var something
 
-function dataPrep() {
 
+function dataPrepLogin() {
+  var doctor_email = document.getElementById('inputEmail4');
+  var doctor_password = document.getElementById('inputPassword4');
+  toast(doctor_email + ", " + doctor_password);
+  return doctor_email, doctor_password
+}
+function dataPrepAppointment() {
+  var patient_email = document.getElementById('patient_email');
+  var bucket_name = 0;
+  var alz_test = document.getElementById('Alz-test-score');
+  var ml_score = document.getElementById('ml-score');
+  var doc_score = document.getElementById('doc_score');
+  return patient_email, bucket_name, alz_test, ml_score, doc_score
+
+}
+function dataPrepPatient() {
+
+}
+function dataPrepRegister() {
+  var f_name = document.getElementById('First_Name');
+  var l_name = document.getElementById('Last_Name');
+  var email = document.getElementById('inputEmail4');
+  var password = document.getElementById('inputPassword4');
+  var address = document.getElementById('inputAddress');
+  var address2 = document.getElementById('inputAddress2');
+  var city = document.getElemementById('inputCity');
+  var state = document.getElementById('inputState');
+  var zip = document.getElementById('inputZip');
+  return f_name, l_name, email, password, address, address2, city, state, zip
 }
 
 function toast(text){
@@ -25,17 +52,51 @@ function toast(text){
 }
 //Insert Appointment into the database
 document.getElementById("insert_appointment").onclick = function() {
-  dataPrep();
+  patient_email, bucket_name, alz_test, ml_score, doc_score = dataPrepAppointment();
   firebase.database().ref('doctor1/Appointments/' + String(Date)).set( {
-    Patient_Name: patient_name,
+    Patient_Email: patient_email,
     Bucket_Name: bucket_name,
-    Alzheimer_Test_Score: test_score,
+    Alzheimer_Test_Score: alz_test,
     ML_Score: ml_score,
     Doctor_Score: doc_score
   })
 };
 
-document.getElementById('some-other-button').onclick = function() {
+//Function to register
+document.getElementById('register_btn').onclick = function() {
+  var f_name, l_name, email, password, address, address2, city, state, zip = dataPrepRegister();
+  toast('Variables collected');
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    //Signed in
+    var user = userCredential.user;
+    //...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.Message;
+  })
 
-};
+}
 
+//Function to login
+document.getElementById('login_button').onclick = function() {
+  var email, password = dataPrepLogin();
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+};  
+
+//Signing Out
+firebase.auth().signOut().then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
