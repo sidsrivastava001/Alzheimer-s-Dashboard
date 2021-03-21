e8fb11aa97f24a2d50e9f6d5b05df8b4419115a6
sidsrivastava001 <49328871+sidsrivastava001@users.noreply.github.com>
2021-03-21 17:40:48 -0400

    Started Navigation

--- Mobile App/MobileApp/App.js
+++ Mobile App/MobileApp/App.js
@@ -1,16 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Button, TextInput} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-
-
-export default function App() {
+function HomeScreen() {
   const [username, setUsername] = useState('');
+  function usernameHandler(text) {
+    setUsername(text);
+  }
+  function buttonHandler() {
+    console.log(username);
+  }
   return (
     <View style={{padding: 30}}>
-      <TextInput placeholder="Enter email address"/>
-      <Button title="Start Tests" onPress={() => console.log("Pressed!")}/>
+      <TextInput placeholder="Enter email address" onChangeText={usernameHandler} value={username}/>
+      <Button title="Start Tests" onPress={buttonHandler}/>
     </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
