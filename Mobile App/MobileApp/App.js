727a961a2b57577a8510450ac16049c44055fa76
sidsrivastava001 <49328871+sidsrivastava001@users.noreply.github.com>
2021-03-23 02:12:03 -0400

    Added Math Test State Logic

--- Mobile App/MobileApp/App.js
+++ Mobile App/MobileApp/App.js
@@ -3,14 +3,16 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Button, TextInput} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import MathQuestions from './Math.js';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   const [username, setUsername] = useState('');
   function usernameHandler(text) {
     setUsername(text);
   }
   function buttonHandler() {
     console.log(username);
+    navigation.navigate('Math');
   }
   return (
     <View style={{padding: 30}}>
@@ -27,6 +29,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Math" component = {MathQuestions} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ Mobile App/MobileApp/Math.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Button, TextInput} from 'react-native';
+var num = 0;
+var questions = [[1, 1, 2], [2, 3, 6], [4, 2, 2], [10, 2, 5]];
+var operators = ['+', '*', '-', '/'];
+var correct = 0;
+export default function MathQuestions() {
+    const [answer, setAnswer] = useState('0');
+    const [time, setTime] = useState(0);
+    
+    const [problem, setProblem] = useState("1+1=");
+    
+    function updateAnswer(ans) {
+        setAnswer(ans);
+    }
+    function updateCorrect() {
+        console.log("Ans: ", answer);
+        if(answer == questions[num][2]) {
+            correct++;
+        }
+        num+=1;
+        console.log("Num: ", num);
+        var prob;
+        if(num == 4) {
+            console.log("Correct: " + correct);
+            prob = "NULL";
+        }
+        else {
+            prob = questions[num][0] + operators[num] + questions[num][1] + "=";
+        }
+        console.log("Problem: ", prob);
+        setAnswer('');
+        setProblem(prob);
+    }
+    return (
+        <View>
+            <View style={{padding: 30}}>
+                <Text>{problem}</Text>
+            </View>
+            <View style={{padding: 30}}>
+                <TextInput placeholder="Enter answer to Math Problem" onChangeText={updateAnswer} value={answer}/>
+                <Button title="Submit Answer" onPress={updateCorrect}/>
+            </View>
+        </View>
+      );
+}
+
